1d3772e1fab53490066e0ed50716ba9c886624d3
Thibautg16 <thibaut@thibaut.ovh>
2018-03-29 01:03:45 +0200

    v7.0

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
 # NetatmoWeatherAppEedomus
+Gestion d'une [station météo Netatmo](https://www.netatmo.com/fr-FR/product/weather/) via la box eedomus
+
+Script créé par [@Thibautg16](https://twitter.com/Thibautg16/)
+
+Dépot GIT : [https://github.com/Thibautg16/NetatmoWeatherAppEedomus/](https://github.com/Thibautg16/NetatmoWeatherAppEedomus/)
+
+Cette application est l'adaptation du script existant avec quelques améliorations (![changelog](https://github.com/Thibautg16/NetatmoWeatherAppEedomus/blob/master/CHANGELOG.md))
+
+## Prérequis 
+Vous devez au préalable disposer d'une station météo Netatmo installée et configurée sur l'application Netatmo.
+
+## Commençons
+### Ajout du périphérique 
+Cliquez sur "Configuration" / "Ajouter ou supprimer un prériphérique" / "Store eedomus" / "Station météo Netatmo" / "Créer"
+
+![creer_peripherique](https://user-images.githubusercontent.com/4451322/37554889-7c1d71e8-29df-11e8-9321-b36d4de12f32.png)
+
+### Configuration du périphérique
+![netatmo_config_peripherique](https://user-images.githubusercontent.com/4451322/37554908-a4014f5e-29df-11e8-841b-48a1c63b2dee.png)
+
+#### Code d'autorisation Oauth :
+Cliquez sur **Cliquez ici pour obtenir votre code code d'autorisation**. Vous êtes alors redirigés vers le portail Netatmo : 
+
+![netatmo_oauth](https://user-images.githubusercontent.com/4451322/34654159-e85a8ada-f3f7-11e7-9e18-d275b62f1595.png)
+
+Vous êtes ensuite redirigés vers le site **Eedomus** : 
+
+![netatmo_oauth_eedomus](https://user-images.githubusercontent.com/4451322/34655047-7a452fac-f404-11e7-91bc-88db549eb881.png)
+
+Copiez le *code d'autorisation Oauth Netatmo* obtenu sur la page eedomus qui est restée ouverte dans votre navigateur Internet ainsi que l'adresse *MAC de votre station météo*. 
+
+#### Voici les différents champs à renseigner :
+
+**Configuration :**
+
+* [Optionnel] - Nom personnalisé : personnalisation du nom de votre périphérique
+* [Obligatoire] - Pièce : vous devez définir dans quelle pièce se trouve votre thermostat
+* [Obligatoire] - Code d'autorisation Oauth Netatmo
+* [Obligatoire] - Adresse MAC de votre station Netatmo
+
+
+**Station Météo :**
+
+* [Optionnel] - Station météo - Co2 : choisissez si vous souhaitez créer ce module 
+* [Optionnel] - Station Météo - Humidité : choisissez si vous souhaitez créer ce module
+* [Optionnel] - Station Météo - Pression : choisissez si vous souhaitez créer ce module
+* [Optionnel] - Station Météo - Niveau Sonore : choisissez si vous souhaitez créer ce module
+* [Optionnel] - Station Météo - Signal wifi : choisissez si vous souhaitez créer ce module 
+* [Optionnel] - Station Méteo - Tendance Température : choisissez si vous souhaitez créer ce module
+* [Optionnel] - Station Météo - Tendance Pression : choisissez si vous souhaitez créer ce module
+
+
+**Module Extérieur :**
+
+* [Optionnel] - Module extérieur - Température : choisissez si vous souhaitez créer ce module
+* [Optionnel] - Module extérieur - Tendance Température : choisissez si vous souhaitez créer ce module 
+* [Optionnel] - Module extérieur - Humidité : choisissez si vous souhaitez créer ce module
+* [Optionnel] - Module extérieur - Signal radio : choisissez si vous souhaitez créer ce module
+* [Optionnel] - Module extérieur - Batterie : choisissez si vous souhaitez créer ce module
+
+
+Plusieurs modules sont créés sur votre box eedomus, ainsi que le script Netatmo. 
+
+![canaux](https://user-images.githubusercontent.com/4451322/36350642-13f9cdbc-149c-11e8-8443-b69420efcf8d.png)
+
+
+## Mise à jour du script
+Si vous possédez déjà le périphérique et que vous souhaitez simplement profiter de la mise à jour du script.
+Dans un premier temps vous rendre dans la configuration de votre périphérique et cliquer sur "Vérifier les mises à jour de netatmo_oauth.php":
+
+![eedomus_script_verif](https://user-images.githubusercontent.com/4451322/36350665-62a23cc4-149c-11e8-9469-e998a90ff8d3.png)
+
+
+Cliquez alors sur "Mettre à jour netatmo_oauth.php avec la dernière version disponible.":
+
+![eedomus_script_maj](https://user-images.githubusercontent.com/4451322/34960084-af7cbb3c-fa39-11e7-8ff1-b31f13cb525d.png)
+
+
+
+![release](https://img.shields.io/github/release/Thibautg16/NetatmoWeatherAppEedomus.svg?style=for-the-badge)
+![licence](https://img.shields.io/github/license/Thibautg16/NetatmoWeatherAppEedomus.svg?style=for-the-badge)
+![status](https://img.shields.io/badge/Status-Prod-green.svg?style=for-the-badge)
+![@Thibautg16](https://img.shields.io/badge/twitter-@Thibautg16-blue.svg?style=for-the-badge)
--- netatmo_oauth.php
+++ netatmo_oauth.php
@@ -1,18 +1,19 @@
 <?php
-// script créé par Connected Object pour eedomus
-// ce script permet de générer un XML facilement lisible par eedomus
-// généré à partir des données Netatmo récupérées via Oauth
-// https://dev.netatmo.com/doc/authentication
-// encodage iso-8859-1 pour les accents
-// Version 1 / 7 avril 2014		  / 1ére version disponible
-// Version 2 / 28 avril 2014	  / Support du pluviomêtre et élimination de requètes inutiles
-// Version 3 / 12 mai 2014	    / Amélioration du support des stations et extensions multiples, introduction d'un cache
-// Version 4 / 18 décembre 2015	/ Gestion de l'anémomêtre
-// Version 5 / 2 avril 2016	    / Pluviométrie sur l'heure et la journée, merci Domo-Blog :)
-// Version 6 / 19 mai 2017	    / Mise à jour API Netatmo
-// Version 6.1 / 20 mai 2017	  / Ajout information Batterie Modules by @Thibautg16
-// Version 6.2 / 30 juin 2017	  / Correction stations multiples
-// Version 6.3 / 11 août 2017   / Ajout informations signal Wifi & RF by @Thibautg16
+# Copyright (C) 2017-2018 @Thibautg16 and Connected Object pour eedomus
+# This file is part of NetatmoWeatherAppEedomus <https://github.com/Thibautg16/NetatmoWeatherAppEedomus>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with This program. If not, see <http://www.gnu.org/licenses/>.
 
 function sdk_deg_to_dir($deg)
 {
@@ -24,11 +25,47 @@ $api_url = 'https://api.netatmo.com';
 $code = getArg('oauth_code');
 $prev_code = loadVariable('code');
 $CACHE_DURATION = 5; // minutes
-$last_xml_success = loadVariable('last_xml_success');
+$device_id = $_GET['device_id'];
+$last_xml_success = loadVariable('last_xml_success'.$device_id);
+
+function sdk_netatmo_check_devices($json_devices){
+	if ($json_devices['error']['code'] == 2 /*invalid access token*/)
+	{
+		// on force l'expiration pour la fois suivante
+		saveVariable('expire_time', 0);
+	}
+	if ($json_devices['error'] != '')
+	{
+		die("Erreur lors de la lecture des devices: <b>".$json_devices['error']['message'].'</b>');
+	}
+}
+
+function sdk_netatmo_devices_html($json_devices){
+	$html='';
+	
+	if(sizeof($json_devices) > 0){
+		$html .= '<br/>';
+		$html .= '<br/>';
+		$html .= 'Voici la liste de vos stations météo Netatmo :';
+		$html .= '<br/>';
+		$html .= '<ul>';
+	}
+
+	for ($i = 0; $i < sizeof($json_devices['body']['devices']); $i++){
+		$html .= '<li><b>'.$json_devices['body']['devices'][$i]['station_name'].'</b>: <input onclick="this.select();" type="text" size="40" readonly="readonly" value="'.$json_devices['body']['devices'][$i]['_id'].'" /></li>';
+	}
+
+	if(sizeof($json_devices) > 0){
+		$html .= '</ul>';
+	}
+
+	return $html;
+}
+
 if ((time() - $last_xml_success) / 60 < $CACHE_DURATION)
 {
 	sdk_header('text/xml');
-	$cached_xml = loadVariable('cached_xml');
+	$cached_xml = loadVariable('cached_xml'.$device_id);
 	echo $cached_xml;
 	die();
 }
@@ -80,8 +117,13 @@ if ($access_token == '')
     die("Erreur lors de l'authentification");
   }
 }
-if ($_GET['mode'] == 'verify')
-{
+if ($_GET['mode'] == 'verify'){
+	$url_devices = $api_url.'/api/getstationsdata?access_token='.$access_token;
+	$result_devices = httpQuery($url_devices);
+	$json_devices = sdk_json_decode($result_devices);
+	sdk_netatmo_check_devices($json_devices);
+	$devices_html=sdk_netatmo_devices_html($json_devices);
+
 	?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
@@ -95,35 +137,36 @@ if ($_GET['mode'] == 'verify')
   </style>
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
   </head><?
-  echo '<br>';
+  echo '<br/>';
 	echo "Votre code d'authentification Netatmo est : ".'<input onclick="this.select();" type="text" size="40" readonly="readonly" value="'.$code.'" />';
-	echo '<br>';
-	echo '<br>';
-	echo "Vous pouvez le copier/coller dans le paramétrage de votre périphérique eedomus.";
+	echo $devices_html;
+	echo "<p>Vous pouvez copier/coller dans le code d'authentification Netatmo et l'id de votre station dans le paramétrage de votre périphérique eedomus.";
+	echo '<br/>';
+	echo "Vous pouvez ensuite fermer cette page.</p>";
 	die();
 }
-$url_devices = $api_url.'/api/getstationsdata?access_token='.$access_token;
+
+// l'utilisateur indique un device_id (si plusieurs station sur le même compte)
+if($device_id != ""){
+	$url_devices = $api_url.'/api/getstationsdata?device_id='.$device_id.'&access_token='.$access_token;
+}
+else{
+	$url_devices = $api_url.'/api/getstationsdata?access_token='.$access_token;
+}
+
 $result_devices = httpQuery($url_devices);
 // gestion offline pour debug
 //saveVariable('result_devices', $result_devices); die();
 //$result_devices = loadVariable('result_devices');
 $json_devices = sdk_json_decode($result_devices);
-if ($json_devices['error']['code'] == 2 /*invalid access token*/)
-{
-  // on force l'expiration pour la fois suivante
-  saveVariable('expire_time', 0);
-}
-if ($json_devices['error'] != '')
-{
-  die("Erreur lors de la lecture des devices: <b>".$json_devices['error']['message'].'</b>');
-}
+sdk_netatmo_check_devices($json_devices);
 $data_store = array();
 // Stations
 $e = 0;
 for ($i = 0; $i < sizeof($json_devices['body']['devices']); $i++)
 {
 	$extension_device[$i]['id'] = $json_devices['body']['devices'][$i]['_id'];
-	$extension_device[$i]['name'] = $json_devices['body']['devices'][$i]['module_name'];
+	$extension_device[$i]['name'] = $json_devices['body']['devices'][$i]['station_name'];
 	$extension_device[$i]['wifi_status'] = $json_devices['body']['devices'][$i]['wifi_status'];
 	$data_store[$json_devices['body']['devices'][$i]['_id']] = $json_devices['body']['devices'][$i]['dashboard_data'];
 	
@@ -194,6 +237,14 @@ function sdk_netatmo_xml($prefix, $data_store, $extension_module)
 		{
 			$ret .= '<humidity>'.$data_store[$id]['Humidity'].'</humidity>';
 		}
+		if (isset($data_store[$id]['temp_trend']))
+		{
+			$ret .= '<temp_trend>'.$data_store[$id]['temp_trend'].'</temp_trend>';
+		}
+		if (isset($data_store[$id]['pressure_trend']))
+		{
+			$ret .= '<pressure_trend>'.$data_store[$id]['pressure_trend'].'</pressure_trend>';
+		}				
 		if (isset($data_store[$id]['Rain']))
 		{
 			$ret .= '<rain>'.$data_store[$id]['Rain'].'</rain>';
@@ -249,7 +300,7 @@ echo $cached_xml;
 $cached_xml = str_replace('<cached>0</cached>', '<cached>1</cached>', $cached_xml);
 if ($xml_content != '') // non vide
 {
-	saveVariable('cached_xml', $cached_xml);
-	saveVariable('last_xml_success', time());
+	saveVariable('cached_xml'.$device_id, $cached_xml);
+	saveVariable('last_xml_success'.$device_id, time());
 }
 ?>
